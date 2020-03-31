54f8044b64cd224992b81213bff3dbc7fe122893
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2020-03-31 11:01:41 -0400

    Adicionado model e view Department

--- SalesWebMVC/SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/HomeController.cs
@@ -5,19 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() 
+        public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Sistema de vendas";
+            ViewData["Message"] = "Your application description page.";
+
             return View();
         }
 
